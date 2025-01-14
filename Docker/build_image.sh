7813cc9c5545fbd181e53959df61b41b6f54a7a6
JunSpring <wjdwnsqja08@gmail.com>
2025-01-14 17:18:42 +0900

    Edit build_image.sh github version is '2.55.1' docker version is 'BFMC'

--- Docker/build_image.sh
+++ Docker/build_image.sh
@@ -1,14 +1,12 @@
 #! /bin/sh
 
-# This script builds docker image of the latest librealsense github tag 
-# Get the latest git TAG version
-LIBRS_GIT_TAG=`git describe --abbrev=0 --tags`
-LIBRS_VERSION=${LIBRS_GIT_TAG#"v"}
+# We commented out and edit some codes below because we don't have tags yet
+# LIBRS_GIT_TAG=`git describe --abbrev=0 --tags`
+# LIBRS_VERSION=${LIBRS_GIT_TAG#"v"}
+# echo "Building images for librealsense version ${LIBRS_VERSION}"
 
-echo "Building images for librealsense version ${LIBRS_VERSION}"
 docker build \
         --target librealsense \
-        --build-arg LIBRS_VERSION=$LIBRS_VERSION \
-        --tag librealsense \
-        --tag librealsense --tag librealsense:$LIBRS_GIT_TAG \
+        --build-arg LIBRS_VERSION="2.55.1" \
+        --tag librealsense:BFMC \
         .
