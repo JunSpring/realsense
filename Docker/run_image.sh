46026a449811a9dd4bd5ef4856a10c1b89c8cfaf
JunSpring <wjdwnsqja08@gmail.com>
2025-01-14 17:19:17 +0900

    Edit run_image.sh Set workspace to current directory

--- Docker/run_image.sh
+++ Docker/run_image.sh
@@ -3,8 +3,12 @@
 # By using --device-cgroup-rule flag we grant the docker continer permissions -
 # to the camera and usb endpoints of the machine.
 # It also mounts the /dev directory of the host platform on the contianer 
+# and shares the current directory with the container.
+
 docker run -it --rm \
     -v /dev:/dev \
+    -v "$(pwd):/workspace" \
     --device-cgroup-rule "c 81:* rmw" \
     --device-cgroup-rule "c 189:* rmw" \
-    librealsense
+    -w /workspace \
+    librealsense:BFMC
